d184a0c74b8353d68b9010132eb18b86003afca4
BomEllen <jjjj200100@gmail.com>
2024-12-06 15:48:27 +0900

    test

--- src/components/ProductList.ts
+++ src/components/ProductList.ts
@@ -1,20 +1,20 @@
 import { LitElement, html, css, CSSResultGroup } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import resetCSS from "../Layout/resetCSS";
-import { Product } from "../@types/type";
+import { Auth, Product } from "../@types/type";
 import { getPbImageURL } from "../api/getPbImageURL";
 import gsap from "gsap";
 
 @customElement("product-list")
 class ProductList extends LitElement {
-  @property({ type: Object }) data: Product = {
-    items: [],
-    page: 0,
-    perPage: 0,
-    totalItems: 0,
-    totalPages: 0,
+  @property({ type: Object }) data:Product = {
+    items:[],
+    page:0,
+    perPage:0,
+    totalItems:0,
+    totalPages:0
   };
-
+  
   @state() loginData = {} as Auth
 
   static styles: CSSResultGroup = [
@@ -65,15 +65,17 @@ class ProductList extends LitElement {
           }
         }
       }
-      .new-post {
+
+      .new-post{
         padding: 0.5rem 1rem;
-        background-color: orange;
-        color: white;
+        background-color: dodgerblue;
+        color:white;
         border-radius: 20px;
         position: fixed;
+        left:50%;
         transform: translateX(-50%);
-        left: 50%;
         bottom: 2rem;
+        
       }
     `,
   ];
@@ -84,63 +86,66 @@ class ProductList extends LitElement {
   }
 
   async fetchData() {
-    const response = await fetch(`${import.meta.env.VITE_PB_API}/collections/products/records`);
+    const response = await fetch(
+      `${import.meta.env.VITE_PB_API}/collections/products/records`
+    );
 
     const data = await response.json();
     this.data = data;
 
-    this.loginData = JSON.parse(localStorage.getItem("auth") ?? "{}");
+    this.loginData = JSON.parse(localStorage.getItem('auth') ?? "{}");
 
+    
   }
 
+  updated(changedProperties: Map<string|number|symbol,unknown>):void{ 
 
-  updated(changedProperties: Map<string | number | symbol, unknown>): void {
-    super.updated(changedProperties);
+    super.updated(changedProperties)
 
-    const item = this.renderRoot.querySelectorAll(".product-item");
+    const item = this.renderRoot.querySelectorAll('.product-item');
 
-    if (item.length > 0) {
-      gsap.from(item, {
-        y: 30,
-        opacity: 0,
-        stagger: 0.2,
-      });
+    if(item.length > 0){
+      gsap.from(item,{
+        y:30,
+        opacity:0,
+        stagger:0.2
+      })
     }
   }
 
   render() {
 
-    const { isAuth } = this.loginData;
-
+    const {isAuth} = this.loginData;
+    
     return html`
       <div class="container">
         <ul>
-          ${this.data.items.map(
-            (item) => html`
-              <li class="product-item">
-                <a href="${
-              isAuth ? `/src/pages/detail/index.html?product=${item.id}`
-              :`/`
-                }">
-                  <figure>
-                    <img src="${getPbImageURL(item)}" alt="" />
-                  </figure>
-                  <span class="brand">${item.brand}</span>
-                  <span class="description">${item.description}</span>
-                  <span class="price">${item.price.toLocaleString()}원</span>
-                  <div>
-                    <span class="discount">${item.discount}%</span>
-                    <span class="real-price">${(item.price - item.price * item.discount * 0.01).toLocaleString()}원</span>
-                  </div>
-                </a>
-              </li>
-            `
-          )}
+        ${
+          this.data.items.map((item)=> html`
+          <li class="product-item">
+            <a href="${
+              isAuth
+              ? `/src/pages/detail/index.html?product=${item.id}`
+              : `/`
+            }">
+              <figure>
+                <img src="${getPbImageURL(item)}" alt="" />
+              </figure>
+              <span class="brand">${item.brand}</span>
+              <span class="description">${item.description}</span>
+              <span class="price">${item.price.toLocaleString()}원</span>
+              <div>
+                <span class="discount">${item.discount}%</span>
+                <span class="real-price">${(item.price - (item.price * item.discount * 0.01)).toLocaleString()}원</span>
+              </div>
+            </a>
+          </li>
+          `)
+        }
         </ul>
       </div>
 
-
-      <a class = new-post href = "/src/pages/newPost/">+ 상품 추가</a>
+      <a class="new-post" href="/src/pages/newPost/">+ 상품추가</a>
     `;
   }
 }
